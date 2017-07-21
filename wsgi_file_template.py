2dc75f5c8f0ff96660dd13d1790afb5cd4e69af2
PythonAnywhere LLP <developers@pythonanywhere.com>
2017-07-21 14:39:57 +0100

    Create wsgi_file_template.py

---
+++ wsgi_file_template.py
@@ -0,0 +1,28 @@
+import _ast
+from pyflakes.checker import Checker
+import bottle
+
+
+def pyflakes_check(code):
+    try:
+        tree = compile(code, "ignored.py", "exec", _ast.PyCF_ONLY_AST)
+    except SyntaxError as syntax_error:
+        return [dict(text='Syntax Error', row=syntax_error.lineno - 1, type='error')]
+    except ValueError as value_error:
+        message = 'Error: {} somewhere in file'.format(value_error.message)
+        return [dict(text=message, row=0, type='error')]
+
+    w = Checker(tree, '')
+    return [
+        dict(text=m.message % m.message_args, row=m.lineno - 1, type='warning')
+        for m in w.messages
+    ]
+
+
+@bottle.route('/', method='POST')
+def hello_world():
+    code = bottle.request.forms.get('code')
+    bottle.response.headers['Access-Control-Allow-Origin'] = 'https://www.pythonanywhere.com'
+    return dict(errors=pyflakes_check(code))
+
+application = bottle.default_app()
