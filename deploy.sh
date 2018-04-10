a93ae342c84203bd9e2bdf5a223eb9291794c84b
Glenn Jones <glenn.jones@pythonanywhere.com>
2018-04-10 12:58:42 +0100

    Fixed deply.sh. by: Glenn

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source `which virtualenvwrapper.sh`
 workon pyflakes
-pip install -U requirements.txt
+pip install -U -r requirements.txt
 cp wsgi_file_template.py /var/www/pyflakes*_wsgi.py
